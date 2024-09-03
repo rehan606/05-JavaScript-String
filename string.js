cc7b42e26ec0effbe5069ddfbcd286b68464d6ee
rehan606 <rehanlemu@gmail.com>
2024-09-03 19:58:42 +0600

    javascript String

---
+++ string.js
@@ -0,0 +1,18 @@
+const name = 'rehan';
+//UpperCase
+console.log("Uppercase", name.toUpperCase());
+//LowerCase
+console.log("LowerCase", name.toLowerCase())
+
+
+//Compare Strig
+const subject = "chemistry";
+const book = "chemIstry";
+
+if( subject.toUpperCase === book.toUpperCase){
+    console.log("I am reading This Book")
+} else {
+    console.log("i can not read this book")
+}
+
+// Remove White Space From string
